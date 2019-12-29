623233b80204b4effef5a3e2a1924dcc0059cbae
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2019-12-29 18:46:15 +1000

    Install boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,5 +13,15 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
+
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
